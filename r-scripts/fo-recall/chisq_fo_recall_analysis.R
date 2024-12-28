3fc352f42ffc15b1a5e01d84dee9f41efab4d396
Jeffrey Pisklak <pisklak@ualberta.ca>
2024-12-28 12:41:14 -0700

    Added fisher-exact tests

--- r-scripts/fo-recall/chisq_fo_recall_analysis.R
+++ r-scripts/fo-recall/chisq_fo_recall_analysis.R
@@ -21,7 +21,7 @@ fo_high_eff <- cramerV(fo_high_tab, digits = 3)
 fo_low_test <- chisq.test(fo_low_tab)
 fo_low_eff <- cramerV(fo_low_tab, digits = 3)
 
-# Post Hoc Analysis
+# Post Hoc Analysis 1 (preferred)-----------------------------------------------
 # Standardized Residuals
 std_res_high <- chisq.test(fo_high_tab)$stdres
 std_res_low <- chisq.test(fo_low_tab)$stdres
@@ -29,6 +29,85 @@ std_res_low <- chisq.test(fo_low_tab)$stdres
 # P-values
 p_high <- pnorm(abs(std_res_high), lower.tail = FALSE) * 2
 p_low <- pnorm(abs(std_res_low), lower.tail = FALSE) * 2
+
+# Post Hoc Analysis 2-----------------------------------------------------------
+
+# Pairwise Fisher-Exact tests - High Value
+
+# No extreme vs Extreme 1st
+h_NO_v_E1 <- fo %>% 
+  filter(FO_context == "High" & condition != "Extreme Last") %>%
+  droplevels()
+h_NO_v_E1 <- xtabs(~ condition + FO_eval, data = h_NO_v_E1)
+h_NO_v_E1 <- fisher.test(h_NO_v_E1)
+
+# No extreme vs Extreme Last
+h_NO_v_EL <- fo %>% 
+  filter(FO_context == "High" & condition != "Extreme 1st") %>%
+  droplevels()
+h_NO_v_EL <- xtabs(~ condition + FO_eval, data = h_NO_v_EL)
+h_NO_v_EL <- fisher.test(h_NO_v_EL)
+
+# Extreme 1st vs Extreme Last
+h_E1_v_EL <- fo %>% 
+  filter(FO_context == "High" & condition != "No Extreme") %>%
+  droplevels()
+h_E1_v_EL <- xtabs(~ condition + FO_eval, data = h_E1_v_EL)
+h_E1_v_EL <- fisher.test(h_E1_v_EL)
+
+
+# Pairwise Fisher-Exact tests - Low Value
+
+# No extreme vs Extreme 1st
+l_NO_v_E1 <- fo %>% 
+  filter(FO_context == "Low" & condition != "Extreme Last") %>%
+  droplevels()
+l_NO_v_E1 <- xtabs(~ condition + FO_eval, data = l_NO_v_E1)
+l_NO_v_E1 <- fisher.test(l_NO_v_E1)
+
+# No extreme vs Extreme Last
+l_NO_v_EL <- fo %>% 
+  filter(FO_context == "Low" & condition != "Extreme 1st") %>%
+  droplevels()
+l_NO_v_EL <- xtabs(~ condition + FO_eval, data = l_NO_v_EL)
+l_NO_v_EL <- fisher.test(l_NO_v_EL)
+
+# Extreme 1st vs Extreme Last
+l_E1_v_EL <- fo %>% 
+  filter(FO_context == "Low" & condition != "No Extreme") %>%
+  droplevels()
+l_E1_v_EL <- xtabs(~ condition + FO_eval, data = l_E1_v_EL)
+l_E1_v_EL <- fisher.test(l_E1_v_EL)
+
+
+# Dataframe of Fisher tests
+
+fish_tests <- tibble(
+  value = c(rep("High", 3), rep("Low", 3)),
+  comparison = rep(c(
+    "No Extreme : Extreme 1st",
+    "No Extreme : Extreme Last",
+    "Extreme 1st : Extreme Last"
+  ), 2),
+  odds_ratio = c(
+    h_NO_v_E1$estimate, h_NO_v_EL$estimate, h_E1_v_EL$estimate,
+    l_NO_v_E1$estimate, l_NO_v_EL$estimate, l_E1_v_EL$estimate
+  ),
+  p_value = c(
+    h_NO_v_E1$p.value, h_NO_v_EL$p.value, h_E1_v_EL$p.value,
+    l_NO_v_E1$p.value, l_NO_v_EL$p.value, l_E1_v_EL$p.value
+  ),
+  CI_low = c(
+    h_NO_v_E1$conf.int[1], h_NO_v_EL$conf.int[1], h_E1_v_EL$conf.int[1],
+    l_NO_v_E1$conf.int[1], l_NO_v_EL$conf.int[1], l_E1_v_EL$conf.int[1]
+  ),
+  CI_upper = c(
+    h_NO_v_E1$conf.int[2], h_NO_v_EL$conf.int[2], h_E1_v_EL$conf.int[2],
+    l_NO_v_E1$conf.int[2], l_NO_v_EL$conf.int[2], l_E1_v_EL$conf.int[2]
+  ),
+)
+
+fish_tests$p_value_adj <- p.adjust(fish_tests$p_value, method = "holm")
 
 
 # https://rcompanion.org/handbook/H_04.html
