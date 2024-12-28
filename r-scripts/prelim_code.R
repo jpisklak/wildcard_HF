6621bc543d7b23955b36e5a0ede53facc379c744
Jeffrey Pisklak <pisklak@ualberta.ca>
2024-12-28 13:00:45 -0700

    Replaced svgs to fix Rmd render issue.

--- r-scripts/prelim_code.R
+++ r-scripts/prelim_code.R
@@ -1,13 +1,13 @@
 # Install and Load Necessary Packages
 pkgs <- c(
-  "tidyverse",
-  "patchwork",
-  "ggh4x",
   "effsize",
   "Hmisc",
   "rcompanion",
   "nlme",
-  "RColorBrewer"
+  "RColorBrewer",
+  "tidyverse",
+  "patchwork",
+  "ggh4x"
 )
 
 installed_pkgs <- pkgs %in% rownames(installed.packages())
